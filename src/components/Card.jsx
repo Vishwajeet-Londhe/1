793907feac4ddab4f24a94cabab33112dc7205d1
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-07-30 23:14:10 +0530

    made rounded shape

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 function Card (){
     return(
-        <div className= 'w-full h-screen bg-red-200'>
-            <div className='px-3 py-4 bg-stone-950 absolute left-1/2 top-1/2 -translate-x-[50%] -transalte-y-[50%] rounded-lg'>
-            
+        <div className= 'w-full h-screen bg-zinc-200'>
+            <div className='px-3 py-4 bg-zinc-100 absolute left-1/2 top-1/2 -translate-x-[50%] -transalte-y-[50%] rounded-lg overflow-hidden'>
+                <div className='w-32 h-32 bg-zinc-300'></div>
+
             </div>
         </div>
     )
