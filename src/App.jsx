ebb7e174cd61552cb86a96a24b7ceaccb52c3d90
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-07-31 18:39:29 +0530

    name using data

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import Card from "./components/Card";
 
 function App(){
-  const calc=8*88/1000
+  const data = ["vishwajeet","Akanksha","Anushka","Apurva"];
   return(
     <div>
-    {calc}
+      {data.map((elem,index) =>(
+        <div key={index} className="px-3 py-4 bg-zinc-300 rounded-md w-fit">
+          {elem}
+        </div>
+      ))}
     </div>
   )
 }
