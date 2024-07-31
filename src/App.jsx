47f293d41f764fe8855a9c3082971c161ba0801b
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-07-31 21:44:38 +0530

    error solve

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,11 @@ import React from "react";
 import Card from "./components/Card";
 
 function App(){
-  const data = ["vishwajeet","Akanksha","Anushka","Apurva"];
   return(
     <div>
-      {data.map((elem,index) =>(
-        <div key={index} className="px-3 py-4 bg-zinc-300 rounded-md w-fit">
-          {elem}
-        </div>
-      ))}
+      <Card/>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
 function Card (){
-    return(
-        <div className= 'w-full h-screen bg-zinc-200'>
-            <div className='w-52 bg-zinc-100 absolute left-1/2 top-1/2 -translate-x-[50%] -translate-y-[50%] rounded-lg overflow-hidden'>
+return(
+        <div className= 'w-full h-screen flex items-center justify-center gap-10 bg-zinc-200'>
+            <div className='w-52 bg-zinc-100 rounded-lg overflow-hidden'>
+                <div className='w-full h-32 bg-zinc-300'>
+                    <img className='w-full h-full object-cover' src="https://images.unsplash.com/photo-1633174524827-db00a6b7bc74?q=80&w=1796&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                </div>
+                <div className='w-full px-3 py-4'>
+                    <h2 className='font-semibold'>amazon</h2>
+                    <p className='text-xs mt-3'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus, libero.</p>
+                </div>
+
+            </div>
+            <div className='w-52 bg-zinc-100 rounded-lg overflow-hidden'>
                 <div className='w-full h-32 bg-zinc-300'>
                     <img className='w-full h-full object-cover' src="https://images.unsplash.com/photo-1633174524827-db00a6b7bc74?q=80&w=1796&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                 </div>
