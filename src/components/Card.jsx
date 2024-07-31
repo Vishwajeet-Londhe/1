e2e4f031d7b2043c337ab5fbb2aeead05e3a2a7c
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-07-31 23:11:43 +0530

    All three cards done

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,28 +1,27 @@
 import React from 'react'
 
 function Card (){
+
+    const data = [
+        {image:'https://images.unsplash.com/photo-1592578629295-73a151d69c96?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' , name: "Amazon Basic" , description: "hello i am amazon"},
+        {image:'https://images.unsplash.com/photo-1483721310020-03333e577078?q=80&w=1856&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', name: "daily exercise", description: "this is daily exercise"},
+        {image:'https://plus.unsplash.com/premium_photo-1689596509894-f9dbcd4f5de9?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', name:"fruits" , description: "this is fruits"},
+    ]
+
 return(
         <div className= 'w-full h-screen flex items-center justify-center gap-10 bg-zinc-200'>
-            <div className='w-52 bg-zinc-100 rounded-lg overflow-hidden'>
+            {data.map((elem, index)=>(
+                <div className='w-52 bg-zinc-100 rounded-lg overflow-hidden'>
                 <div className='w-full h-32 bg-zinc-300'>
-                    <img className='w-full h-full object-cover' src="https://images.unsplash.com/photo-1633174524827-db00a6b7bc74?q=80&w=1796&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                    <img className='w-full h-full object-cover' src={elem.image} alt="" />
                 </div>
                 <div className='w-full px-3 py-4'>
-                    <h2 className='font-semibold'>amazon</h2>
-                    <p className='text-xs mt-3'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus, libero.</p>
+                    <h2 className='font-semibold'>{elem.name}</h2>
+                    <p className='text-xs mt-3'>{elem.description}</p>
                 </div>
-
             </div>
-            <div className='w-52 bg-zinc-100 rounded-lg overflow-hidden'>
-                <div className='w-full h-32 bg-zinc-300'>
-                    <img className='w-full h-full object-cover' src="https://images.unsplash.com/photo-1633174524827-db00a6b7bc74?q=80&w=1796&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-                </div>
-                <div className='w-full px-3 py-4'>
-                    <h2 className='font-semibold'>amazon</h2>
-                    <p className='text-xs mt-3'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus, libero.</p>
-                </div>
-
-            </div>
+            ))}
+            
         </div>
     )
 }
